083281dd92058558a93ae84032d32fc4b1f56359
Siranjeevi <136801343+Siranjeevi369@users.noreply.github.com>
2023-07-20 21:18:49 +0530

    Omnifood

---
+++ omnifood/script.js
@@ -0,0 +1,16 @@
+ const sectionHeroEl = document.querySelector(".section-hero");
+ const obs = new IntersectionObserver(function(entries){
+    const ent = entries[0];
+    console.log(ent);
+    if(ent.isIntersecting === false){
+        document.querySelector(".header").classList.add("sticky");
+    }
+    if(ent.isIntersecting === true){
+        document.querySelector(".header").classList.remove("sticky");
+    }
+ },{
+    root:null,
+    threshold:0,
+    rootMargin: "-80px"
+ });
+ obs.observe(sectionHeroEl);
